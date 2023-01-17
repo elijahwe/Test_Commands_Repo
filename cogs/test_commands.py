514dd6221e6169a23fdef5e42e81a8663d64f35c
elijahwe <49080930+elijahwe@users.noreply.github.com>
2023-01-17 00:04:44 -0500

    Moved test commands to cogs folder

---
+++ cogs/test_commands.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+class Test_Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="test1")
+    async def test1(self, ctx: commands.Context):
+        await ctx.send("Test 1 successful")
+        
+    @commands.command(name="test2")
+    async def test1(self, ctx: commands.Context):
+        await ctx.send("Test 2 successful")
+
+async def setup(bot):
+    await bot.add_cog(Test_Commands(bot))
